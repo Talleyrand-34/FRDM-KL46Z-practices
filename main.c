9c83e23b2e4f08e36509499d4620d4d6b501fc2d
Talleyrand-34 <t34@t34.dev>
2025-02-18 05:46:50 +0100

    fast switching

--- main.c
+++ main.c
@@ -3,82 +3,84 @@
 // LED (RG)
 // LED_GREEN = PTD5
 // LED_RED = PTE29
+// SW1 = PTC3
+// SW3 = PTC12
 
 void delay(void)
 {
-  volatile int i;
-
-  for (i = 0; i < 1000000; i++);
+    volatile int i;
+    for (i = 0; i < 100000; i++);
 }
 
-/* // LED_GREEN = PTD5 */
-/* void led_green_init() */
-/* { */
-/*   SIM->COPC = 0; */
-   /* SIM->SCGC5|=1<<12; */
-/*   SIM_SCGC5_PORTD(1); */
-/**/
-/*   PORTD->PCR[5]=1; */
-/*    */
-   /* GPIOD->PDDR */
-/*   // usar PSOR */
-   /* GPIOD->PSOR */
-/* } */
-/**/
-/* void led_green_toggle() */
-/* { */
-/*   // */
-/* } */
-/**/
-/* // LED_RED = PTE29 */
-/* void led_red_init() */
-/* { */
-/*   // */
-/* } */
-/**/
-/* void led_red_toggle(void) */
-/* { */
-/*   // */
-/* } */
-
-// LED_GREEN = PTD5
-void led_green_init()
+void led_green_init(void)
 {
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
-  PORTD->PCR[5] = PORT_PCR_MUX(1);
-  PTD->PDDR |= (1u << 5);
+    SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
+    PORTD->PCR[5] = PORT_PCR_MUX(1);
+    PTD->PDDR |= (1u << 5);
 }
 
-void led_green_toggle()
+void led_red_init(void)
 {
-  PTD->PTOR = (1u << 5);
+    SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+    PORTE->PCR[29] = PORT_PCR_MUX(1);
+    PTE->PDDR |= (1u << 29);
 }
 
-// LED_RED = PTE29
-void led_red_init()
+void switches_init(void)
 {
-  SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-  PORTE->PCR[29] = PORT_PCR_MUX(1);
-  PTE->PDDR |= (1u << 29);
+    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+    PORTC->PCR[3] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+    PORTC->PCR[12] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+    PTC->PDDR &= ~((1u << 3) | (1u << 12));
+}
+
+void led_green_toggle(void)
+{
+    PTD->PTOR = (1u << 5);
 }
 
 void led_red_toggle(void)
 {
-  PTE->PTOR = (1u << 29);
+    PTE->PTOR = (1u << 29);
 }
-
 
 int main(void)
 {
-  led_green_init();
-  led_red_init();
+    SIM->COPC = 0;
+    
+    led_green_init();
+    led_red_init();
+    switches_init();
+    
+    int green_state = 0;
+    int red_state = 0;
 
-  while (1) {
-    led_green_toggle();
-    delay();
-    led_red_toggle();
-  }
+    while (1) {
+        if (!(PTC->PDIR & (1u << 3))) {
+            delay();
+            if (!(PTC->PDIR & (1u << 3))) {
+                green_state = !green_state;
+                if (green_state) {
+                    PTD->PCOR = (1u << 5);
+                } else {
+                    PTD->PSOR = (1u << 5);
+                }
+            }
+        }
+        
+        if (!(PTC->PDIR & (1u << 12))) {
+            delay();
+            if (!(PTC->PDIR & (1u << 12))) {
+                red_state = !red_state;
+                if (red_state) {
+                    PTE->PCOR = (1u << 29);
+                } else {
+                    PTE->PSOR = (1u << 29);
+                }
+            }
+        }
+    }
 
-  return 0;
+    return 0;
 }
+
